a143be87051b03428d85d7ffd556f957aad285b7
etiescu <tiernanscully@gmail.com>
2021-09-27 13:58:56 +0200

    listen on port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, get, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello World!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let address = match std::env::var("PORT") {
+        Ok(port) => format!("0.0.0.0:{}", port),
+        Err(_e) => "127.0.0.1:8080".into()
+    };
+    HttpServer::new(|| App::new().service(index))
+        .bind(&address)?
+        .run()
+        .await
 }
